e0f8328a5b3c94229487d0b9c9678f3704753730
Heather Smith <halexsmith86@gmail.com>
2021-08-10 14:05:18 -0400

    linked jquery, started shareToggle function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// need a function for the active state on mobile -- a footer needs to be unhidden and to pop up to cover michelle's name, pic, etc.
+// targets the share buttons
+// let shareArrow = document.querySelectorAll("#arrow #footer");
 
-//
+// takes in the click event, hides the author section, and unhides the footer
+let shareToggle = () => {
+    var author = document.querySelector('#author');
+    var footer = document.querySelector('#footer');
+    if(footer.style.display === "none") {
+        footer.style.display = "block";
+    } else {
+        footer.style.display = "none";
+        author.style.display = "block";
+    }
+}
