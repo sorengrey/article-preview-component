357d37c5201b66765ee1cdb4d6f0e118faf03f98
Heather Smith <halexsmith86@gmail.com>
2021-08-11 14:26:51 -0400

    styled footer in desktop view, added to showFooter()

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var author = document.querySelector('#author');
 var footer = document.querySelector('#footer');
 
-// hides the author section, shows the footer
+// hides the author section, shows the footer - keeps the author section visible in desktop view
 function showFooter() {
     footer.style.display = "block";
     author.style.display = "none";
+    if(footer.style.position = "absolute"){
+        author.style.display = "flex";
+    }
 }
 
 // hides the footer, shows the author section
