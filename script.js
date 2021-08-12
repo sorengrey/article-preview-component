244f1a4d6570b074526be2eda564bf2737c4d997
Heather Smith <halexsmith86@gmail.com>
2021-08-12 14:41:55 -0400

    added window width condition to showFooter()

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ var arrow2 = document.querySelector('#arrow2');
 // hides the author section, shows the footer - keeps the author section visible in desktop view when share arrow is clicked
 function showFooter() {
     footer.style.display = "block";
+    footer.style.position = "auto";
     author.style.display = "none";
     triangle.style.display = "block";
-    if(footer.style.position = "absolute"){
+    if($(window).width() > 376){
         author.style.display = "flex";
         arrow2.style.display = "block";
         arrow.style.display = "none";
